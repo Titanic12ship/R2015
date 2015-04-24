20c5dbe16b7ff08ee95a92b03554925da89e50bd
rupc0015 <rupc0015@RobiLab312-06.rowanads.rowan.edu>
2015-04-24 09:32:28 -0400

    3

---
+++ Rowan Contest/src/Icon.java
@@ -0,0 +1,17 @@
+
+public class Icon {
+	
+	private int id;
+	private int x, y; // offset from the top and left of the screen
+	private int squareLength;
+	
+	
+	public Icon(int id, int x, int y, int length)
+	{
+		this.id = id;
+		this.x = x;
+		this.y= y;
+		this.squareLength = length;
+	}
+	
+}
--- Rowan Contest/src/Main.java
+++ Rowan Contest/src/Main.java
@@ -8,6 +8,16 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		
+		int dataSets;
+		int screens;
+		String screenInfo;
+		String iconInfo; // for each icon
+		String mouseSettings;
+		int mouseEvents;
+		String mouseEventData;
+		
+		
+		
 		final String FILE_NAME = "src/test.txt";
 		File f = new File(FILE_NAME);
 		Scanner fScan, lScan;
